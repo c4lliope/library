59830d07672b1c29d375657dcd3f224c0f38a3b4
Grace Youngblood <grace.c.youngblood@gmail.com>
2020-05-11 16:15:27 -0700

    Style modal a bit.

--- app/javascript/packs/blocks/modal.js
+++ app/javascript/packs/blocks/modal.js
@@ -1,10 +1,13 @@
+import React from "react"
 import BaseModal from "styled-react-modal"
 
 const Modal = BaseModal.styled`
-background-color: lightgrey;
+background-color: #fbeed1;
 padding: 1rem;
 max-height: 60vh;
 max-width: 40vw;
+border: 4px solid darkgrey;
+border-radius: 8px;
 `
 
-export default Modal
+export default (props) => <Modal {...props} allowScroll />
