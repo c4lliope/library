9837c485805123358e091d1724542212664f45b7
Grace Youngblood <grace.c.youngblood@gmail.com>
2020-05-14 22:31:41 -0700

    Fix landing rules email bug.

--- app/mailers/hold_mailer.rb
+++ app/mailers/hold_mailer.rb
@@ -9,6 +9,6 @@ class HoldMailer < ApplicationMailer
     def landing_rules
         @hold = params[:hold]
         @landing_address = "http://#{ENV["APPLICATION_HOST"]}/holds/#{@hold.landing_code}"
-        mail(to: @hold.member, subject: "Our Shared Library: confirm receipt.")
+        mail(to: @hold.member.email, subject: "Our Shared Library: confirm receipt.")
     end
 end
