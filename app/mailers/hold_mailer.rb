f4318579ff632dcba7b5ddeb41ed5440ac6dedd0
Grace Youngblood <grace.c.youngblood@gmail.com>
2020-06-26 19:28:38 -0400

    change email messaging.

--- app/mailers/hold_mailer.rb
+++ app/mailers/hold_mailer.rb
@@ -3,12 +3,12 @@ class HoldMailer < ApplicationMailer
 
     def mailing_rules
         @hold = params[:hold]
-        mail(to: @hold.record.member.email, subject: 'Our Shared Library: a reservation.')
+        mail(to: @hold.record.member.email, subject: 'Our Shared Library: a hold has been placed.')
     end
 
     def landing_rules
         @hold = params[:hold]
         @landing_address = "http://#{ENV["APPLICATION_HOST"]}/holds/#{@hold.landing_code}"
-        mail(to: @hold.member.email, subject: "Our Shared Library: confirm receipt.")
+        mail(to: @hold.member.email, subject: "Our Shared Library: your hold is pending.")
     end
 end
